89f3ae36454a024bc02cac76175a1b42a734d965
Danielle <daniellegarratt2304@hotmail.co.uk>
2023-05-01 18:50:15 +0100

    worked on tabs but will be needing to change and not liking what I have done

--- src/assets/js/CV-tab.js
+++ src/assets/js/CV-tab.js
@@ -1,16 +1,9 @@
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+$('#myTabs').bind('show', function(e) {  
+  paneID = $(e.target).attr('href');
+  src = $(paneID).attr('data-src');
+  // if the iframe hasn't already been loaded once
+  if($(paneID+" iframe").attr("src")=="")
+  {
+      $(paneID+" iframe").attr("src",src);
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+});
